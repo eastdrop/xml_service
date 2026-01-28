72a4686797d3f7c713b726758fbbceae8a50dad9
eastdrop <lyokha7777@gmail.com>
2026-01-28 21:25:46 +0300

    initial commit

---
+++ src/main/java/com/xmlservice/xmlservice/XmlserviceApplication.java
@@ -0,0 +1,13 @@
+package com.xmlservice.xmlservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XmlserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XmlserviceApplication.class, args);
+	}
+
+}
